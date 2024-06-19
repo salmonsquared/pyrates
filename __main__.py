8aa89ba697ebad56b8dac781c60bd6b037b6f2fe
Jay <136963523+fishcreature@users.noreply.github.com>
2024-06-19 15:08:07 +1200

    basic class made utils

--- __main__.py
+++ __main__.py
@@ -1,2 +1,7 @@
 import utils
+import tkinter
+from ctypes import windll
 
+windll.shcore.SetProcessDpiAwareness(1) # Fixes blurry text on W11
+
+tkinter._test()
---
+++ utils.py
@@ -0,0 +1,5 @@
+class Review: 
+    def __init__(self, game, stars, review):
+        self.game = game
+        self.stars = stars
+        self.review = review
