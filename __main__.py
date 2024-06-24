d515f7440bee6f4879f7cb25efa988f8f096949d
Jay <136963523+fishcreature@users.noreply.github.com>
2024-06-24 12:27:42 +1200

    Basic Tkinter Structure

--- __main__.py
+++ __main__.py
@@ -1,10 +1,24 @@
-import utils
+import utils # Local Package
 import tkinter as tk
 from ctypes import windll
 
 windll.shcore.SetProcessDpiAwareness(1) # Fixes blurry text on W11
 
-window = tk.Tk()
-greeting = tk.Label(text="Hello World")
-greeting.pack()
-window.mainloop()
+cookies = 0
+
+def when_clicked():
+    global cookies
+    cookies += 1
+    cookie_text.set(str(cookies))
+
+# Initalize Windows
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, width=800, height=500)
+        self.parent = parent
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title('PyRates')
+    MainApplication(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()
