bd222735cd154ee334f3f286a727c5fdf371a9da
Jay <136963523+fishcreature@users.noreply.github.com>
2024-08-02 10:11:23 +1200

    Added API URL and started to work on enter detection

--- __main__.py
+++ __main__.py
@@ -1,38 +1,47 @@
 import utils # Local Package
 import tkinter as tk
+import requests
+import json
 from ctypes import windll
 
 windll.shcore.SetProcessDpiAwareness(1) # Fixes blurry text on W11
+url = "http://www.omdbapi.com/?apikey=7c6a9526&"
+response = requests.get(url)
 
+def enter_pressed(parent):
+    parent.delete(0, 'end')
+    
 # Initalize Windows
 class MainApplication(tk.Frame): #Whole UI 
     def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, width=800, height=500)
+        tk.Frame.__init__(self, parent)
         self.parent = parent
 
-        parent.columnconfigure(1, weight=1, minsize=100)
-        parent.rowconfigure(1, weight=1, minsize=75)
+        parent.columnconfigure(1, weight=1)
+        parent.rowconfigure(1, weight=1)
 
-        graph_label = tk.Label(parent, text="Graph")
-
-        c1 = tk.Label(parent, text = "Empty", background = 'yellow')
-        c2 = tk.Label(parent, text = "Empty", background = 'yellow')
+        graph_label = tk.Label(parent, text="Graph", borderwidth=1, relief='solid')
 
         graph_label.grid(row=0, column=1, sticky='nswe', pady=2)
-        c1.grid(row=1, column=0)
-        DisplayedGrid(self).grid(row=1, column=1, sticky = 'nswe', pady = 2)
-        c2.grid(row=1, column=2)
+        DisplayedGrid(self).grid(row=1, column=1, sticky = 'nswe', pady = 2, padx = 50)
 
 class DisplayedGrid(tk.Frame): #Inner Displayed Rating Graph
     def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, borderwidth=1)
+        tk.Frame.__init__(self, parent)
         self.parent = parent
         for x in range(5):
             parent.columnconfigure(x, weight=1, minsize=200)
             parent.rowconfigure(x, weight=1, minsize=150)
+            header = tk.Label(text="Header")
             for y in range(5):
-                title = tk.Label(parent, text = "Cell", borderwidth=1, relief='solid')
+                title = PosterCell(parent)
                 title.grid(row=x, column=y, sticky = 'nswe')
+
+class PosterCell(tk.Entry):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Entry.__init__(self, parent)
+        self.parent = parent
+        self.bind("<Enter>",enter_pressed(self))
 
 if __name__ == "__main__":
     root = tk.Tk()
