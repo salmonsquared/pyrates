b0b5c12f4f516eeebc9f5e6055d23e126fe8d970
Jay <136963523+fishcreature@users.noreply.github.com>
2024-10-13 14:38:31 +1300

    tkinter -> pyqt

--- __main__.py
+++ __main__.py
@@ -1,70 +1,81 @@
 import utils # Local Package
-import tkinter as tk
+import sys
 import requests
 import json
 from ctypes import windll
+from io import BytesIO
+
+from PyQt5 import QtCore, QtWidgets, Qt
+from PyQt5.QtWidgets import (QApplication, QDialog, QMainWindow, QMessageBox, QTableWidget)
+from PyQt5.uic import loadUi
+
+from main_ui import Ui_MainWindow
 
 windll.shcore.SetProcessDpiAwareness(1) # Fixes blurry text on W11
 movie_api = "http://www.omdbapi.com/?apikey=7c6a9526&"
 
-# Initalize Windows
-class MainApplication(tk.Frame): #Whole UI 
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent)
-        self.parent = parent
+def fetch_image(url):
+    response = requests.get(url)
+    image_data = Image.open(BytesIO(response.content))
+    return ImageTk.PhotoImage(image_data)
 
-        parent.columnconfigure(1, weight=1)
-        parent.rowconfigure(1, weight=1)
+class MyTableWidget(QTableWidget):
+    def __init__(self, parent=None):
+        super(MyTableWidget, self).__init__(parent)
+        self.setEnabled(True)
+        self.setGeometry(QtCore.QRect(32, 40, 381, 221))
+        self.setAutoFillBackground(False)
+        self.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustIgnored)
+        self.setGridStyle(QtCore.Qt.SolidLine)
+        self.setRowCount(5)
+        self.setColumnCount(5)
+        self.setObjectName("tableWidget")
+        item = QtWidgets.QTableWidgetItem()
+        self.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.setHorizontalHeaderItem(4, item)
+        self.horizontalHeader().setVisible(True)
+        self.horizontalHeader().setDefaultSectionSize(60)
+        self.verticalHeader().setVisible(False)
 
-        graph_label = tk.Label(parent, text="Graph", borderwidth=1, relief='solid')
-
-        graph_label.grid(row=0, column=1, sticky='nswe', pady=2)
-        DisplayedGrid(self).grid(row=1, column=1, sticky = 'nswe', pady = 2, padx = 50)
-
-class DisplayedGrid(tk.Frame): #Inner Displayed Rating Graph
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent)
-        self.parent = parent
-        for x in range(5):
-            parent.columnconfigure(x, weight=1, minsize=200)
-            parent.rowconfigure(x, weight=1, minsize=150)
-            header = tk.Label(text="Header")
-            for y in range(5):
-                title = PosterCell(parent)
-                title.grid(row=x, column=y, sticky = 'nswe')
-
-class PosterCell(tk.Entry):
-    def __init__(self, parent, *args, **kwargs):
-        tk.Entry.__init__(self, parent)
-        self.parent = parent
-
-        def enter_pressed(x):
-            movie_title = self.get()
-            print(movie_title)
-            search_parameters = {'t': movie_title}
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == 16777220:
+            print("vvvvvvvv")
+            print(self.currentItem())
+            print(search_title)
+            search_parameters = {'t': search_title}
             movie_data = requests.get(movie_api, params = search_parameters).json()
             print(movie_data)
-            movie_poster = movie_data['Poster']
-            print(movie_poster)
-        self.bind("<Return>", enter_pressed)
+            movie_title = movie_data['Title']
+            print(movie_title)
+            self.delete(0, tk.END)
+            self.insert(0, movie_title)
+            #Poster 
+            movie_poster_url = movie_data['Poster']
+            movie_poster = fetch_image(movie_poster_url)
+            
+            poster_label = tk.Label(root, image=movie_poster)
+        else:
+            super(MyTableWidget, self).keyPressEvent(event)
 
-if __name__ == "__main__":
-    root = tk.Tk()
 
-    menubar = tk.Menu(root)      
-    file = tk.Menu(menubar, tearoff = 0)
-    menubar.add_cascade(label ='File', menu = file)
-    file.add_command(label = "New Table...")
-    file.add_command(label = "Save")
-    file.add_command(label = "Open...")
 
-    help = tk.Menu(menubar, tearoff = 0)
-    menubar.add_cascade(label ='Help', menu = help)
-    help.add_command(label = "Help")
-    help.add_command(label = "Github")
-    help.add_command(label = "About")
 
-    root.title('PyRates')
-    root.config(menu = menubar)
-    MainApplication(root).grid(row = 1, column = 1)
-    root.mainloop()
+# Initalize Windows
+class MainWindow(QMainWindow, Ui_MainWindow): #Whole UI 
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+app = QApplication(sys.argv)
+
+window = MyTableWidget()
+window.show()
+app.exec()
